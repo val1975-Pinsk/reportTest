95c105916214b36ac6e910ba3966aee2bd7844eb
Valentin <bvl2004@mail.ru>
2024-05-08 09:32:47 +0300

    Отлажена работа с файловой системой

--- reportTest.py
+++ reportTest.py
@@ -1,4 +1,17 @@
 import os, fnmatch
+
+def getReportData():
+	while(True):
+		reportDataFile = "/home/valentin/python_dev/report/reportTest/" + input("Название файла отчёта(.txt): ")
+		try:
+			with open(reportDataFile, encoding = "utf8") as file:
+				data = file.readlines()
+				return data
+		except FileNotFoundError:
+			answer = input("Ошибка. Попробовать ещё раз (y/n)? ")
+			if answer == "n" or answer == "н":
+				return "finish"
+	
 '''
 	Получаем список файлов в текущем директории.
 Выбираем файлы с расширением *.txt.
@@ -8,5 +21,5 @@ pattern = "*.txt"
 for entry in listOfFiles:
 	if fnmatch.fnmatch(entry, pattern):
 		print(entry)
-reportData = getReportData(input("Название файла отчёта(.txt): "))
-
+reportData = getReportData()
+print(reportData)
