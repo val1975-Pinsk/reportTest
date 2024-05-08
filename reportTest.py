069920c1a75bcb68b48ad8a147d0a63ad330c14b
Valentin <bvl2004@mail.ru>
2024-05-08 09:57:32 +0300

    Изменён ввод названия файла. Нужно вводить только название, без расширения.

--- reportTest.py
+++ reportTest.py
@@ -2,11 +2,14 @@ import os, fnmatch
 
 def getReportData():
 	while(True):
-		reportDataFile = "/home/valentin/python_dev/report/reportTest/" + input("Название файла отчёта(.txt): ")
+		reportDataFile = "/home/valentin/python_dev/report/reportTest/" + input("Название файла отчёта(.txt): ") + ".txt"
 		try:
 			with open(reportDataFile, encoding = "utf8") as file:
 				data = file.readlines()
-				return data
+				reportData = []
+				for string in data:
+					reportData.append(string.strip())
+				return reportData
 		except FileNotFoundError:
 			answer = input("Ошибка. Попробовать ещё раз (y/n)? ")
 			if answer == "n" or answer == "н":
@@ -22,4 +25,8 @@ for entry in listOfFiles:
 	if fnmatch.fnmatch(entry, pattern):
 		print(entry)
 reportData = getReportData()
-print(reportData)
+if reportData == "finish":
+	print("Работа программы закончена.")
+else:
+	for string in reportData:
+		print(string)
