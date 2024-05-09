9062938ed5d335ae1d545ed94dee53f36d000d81
Valentin <bvl2004@mail.ru>
2024-05-09 13:36:59 +0300

    Допоолнил reportClass. Добавил функцию stringIsHeader

--- reportClass.py
+++ reportClass.py
@@ -1,3 +1,20 @@
 class directReport:
-	def __init__(self, name):
-		self.name = name
+	def __init__(self, direction, date, time):
+		self.direction = direction
+		self.date = date
+		self.time = time
+	
+	def display(self):
+		print(f"Направление: {self.direction}\nДата: {self.date}\nВремя отправления: {self.time}")
+
+
+def convertDate(date):
+	month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+	subDate = date.split("-")
+	return subDate[2] + " " + month[int(subDate[1]) - 1] + " " + subDate[0]
+	
+	
+def newDirReport(string):
+	string = string[16:len(string)-5]
+	words = string.split(", ")
+	return directReport(words[2], convertDate(words[0]), words[1])
--- reportTest.py
+++ reportTest.py
@@ -1,4 +1,5 @@
 import os, fnmatch
+from reportClass import *
 
 def isGetString(string):
 	words = ["Пинск", "свободно", "д.к.", "дк", "Дк", "Д.К.", "безнал", "б/н", "17р", "ребенок", "width=\"25px\"", "selected=\"\">Поехал"]
@@ -22,6 +23,13 @@ def getReportData():
 			answer = input("Ошибка. Попробовать ещё раз (y/n)? ")
 			if answer == "n" or answer == "н":
 				return "finish"
+
+
+def sringIsHeader(string):
+	if "Пинск" in string:
+		return True
+	else: return False
+
 	
 '''
 	Получаем список файлов в текущем директории.
@@ -36,5 +44,9 @@ reportData = getReportData()
 if reportData == "finish":
 	print("Работа программы закончена.")
 else:
+	#	Составление отчёта.
+	report = []
 	for string in reportData:
-		print(string)
+		if sringIsHeader(string):
+			dirReport = newDirReport(string)
+			dirReport.display()
