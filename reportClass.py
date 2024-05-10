40717a4b788a83d1fad25d604d85479deb83c987
Valentin <bvl2004@mail.ru>
2024-05-10 08:03:23 +0300

    Добавил class Report - сводный отчёт

--- reportClass.py
+++ reportClass.py
@@ -1,15 +1,31 @@
+class Report:
+	def __init__(self):
+		self.body = []
+
+
 class directReport:
 	def __init__(self, direction, date, time):
 		self.direction = direction
 		self.date = date
 		self.time = time
+		self.auto = "undefined"
+		self.occupied = 0
+		self.freely = 0
 		self.fullyPrice = Payment(35)
 		self.discounted = Payment(30)
 		self.halfTheCost = Payment(17)
 	
 	def display(self):
 		print(f"Направление: {self.direction}\nДата: {self.date}\nВремя отправления: {self.time}")
+		print(f"Занято мест: {self.occupied}\nСвободных мест: {self.freely}\nАвтомобиль: {self.auto}")
 
+	def setSubHeader(self, string):
+		string = string[16:len(string)-5]
+		words = string.split(", ")
+		self.occupied = words[0][:len(words[0])-2]
+		self.freely = words[1][:len(words[0])-12]
+		self.auto = words[2]
+		
 class Payment:
 	def __init__(self, value):
 		self.value = value
