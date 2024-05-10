70bbe3d5bc09ea5ac576f4ffb8ebc8584e7aeeab
Valentin <bvl2004@mail.ru>
2024-05-10 06:52:01 +0300

    В class directReport добавил свойства оплата. Добаввил class Payment

--- reportClass.py
+++ reportClass.py
@@ -3,8 +3,14 @@ class directReport:
 		self.direction = direction
 		self.date = date
 		self.time = time
+		self.fullyPrice = Payment(35)
+		self.discounted = Payment(30)
+		self.halfTheCost = Payment(17)
 	
 	def display(self):
 		print(f"Направление: {self.direction}\nДата: {self.date}\nВремя отправления: {self.time}")
 
-
+class Payment:
+	def __init__(self, value):
+		self.value = value
+		self.count = 0
